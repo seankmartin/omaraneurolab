7bb4205d9ac9af208cc4d4c55e67e07dc1e095c7
Sean Martin <seankieran.m@hotmail.com>
2021-06-23 11:15:27 +0100

    barnard change

--- ATNx/barnard.r
+++ ATNx/barnard.r
@@ -1,10 +1,13 @@
-# install.packages("Exact", repos="https://cloud.r-project.org")
+install.packages("Exact", repos="https://cloud.r-project.org")
+
 library("Exact")
 
+method_to_use <- "Z-unpooled"
+
 f_obs <- matrix(c(15, 0, 38, 47), nrow=2, ncol=2, byrow=TRUE)
-result <- exact.test(f_obs, alternative="two.sided", method="csm", model="Binomial", cond.row=FALSE)
+result <- exact.test(f_obs, alternative="two.sided", method=method_to_use, model="Binomial", cond.row=FALSE)
 result
 
 f_obs <- matrix(c(16, 13, 37, 34), nrow=2, ncol=2, byrow=TRUE)
-result <- exact.test(f_obs, alternative="two.sided", method="csm", model="Binomial", cond.row=FALSE)
+result <- exact.test(f_obs, alternative="two.sided", method=method_to_use, model="Binomial", cond.row=FALSE)
 result
